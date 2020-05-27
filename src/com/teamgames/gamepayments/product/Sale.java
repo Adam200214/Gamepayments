3cf267fc70e2632d06cf284f84c1ec1922f0d7ea
NULL <abandonedpk@gmail.com>
2020-05-27 17:38:55 +0100

    Sale Getters fixed

--- src/com/teamgames/gamepayments/product/Sale.java
+++ src/com/teamgames/gamepayments/product/Sale.java
@@ -1,10 +1,20 @@
 package com.teamgames.gamepayments.product;
 
-import lombok.Getter;
-
-@Getter
 public class Sale {
     private String saleType;
+
+    public String getSaleType() {
+        return saleType;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public int getDiscountPercentage() {
+        return discountPercentage;
+    }
+
     private double discountAmount;
     private int discountPercentage;
 }
